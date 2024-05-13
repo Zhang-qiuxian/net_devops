3dadb81feecba9bcdaf7688dc680a41a8629bb37
Zhang_qiuxian <1473464511@qq.com>
2024-05-13 16:36:48 +0800

    完成ip interface serial system创建

--- django_server/apps/device/tasks.py
+++ django_server/apps/device/tasks.py
@@ -25,26 +25,39 @@ POOL = 50
 通用的启动函数
 """
 
-snmp_objs = SnmpTemplate.objects.all()
-
 
 def start_snmp(device: Device = None, oids: list = None) -> tuple[bool, list[dict]]:
     """
-    运行snmp的启动函数
+    运行snmp的启动函数,返回值示例
+    (True, [
+    {
+        'serial': [{}],
+        'device_ip': '1.1.1.1',
+        'device_id': 'be541815-1098-47fc-8cbe-cfa84ab6f8ec',
+        'device_name': '11F交换机'
+    }, {
+        'ip': [{}],
+        'device_ip': '1.1.1.1',
+        'device_id': 'be541815-1098-47fc-8cbe-cfa84ab6f8ec',
+        'device_name': '11F交换机'
+    }, {
+        'system': [{}],
+        'device_ip': '1.1.1.1',
+        'device_device_id': 'be541815-1098-47fc-8cbe-cfa84ab6f8ec',
+        'device_name': '11F交换机'
+    }, {
+        'interface': [{}],
+        'device_ip ': '1.1.1.1',
+        'device_device_id ': 'be541815-1098-47fc-8cbe-cfa84ab6f8ec',
+        'device_name ': '11F交换机'
+    }
+])
     :param snmps: snmp模板列表
     :param device: Device模型对象
     :param oids: 需要获取的oids列表
     :return: 返回一个元组，第一个值是布尔值标识是否完成，第二个值是获取到的数据
-    (True,  [{'serial': [
-{'entPhysicalDescr': 'HUAWEI S5720 Routing Switch', 'entPhysicalName': 'HUAWEI S5720 Routing Switch',
- 'entPhysicalSerialNum': '2102350BDQDML5000621', 'entPhysicalSoftwareRev': 'Version 5.170 V200R019C10SPC500',
- 'entPhysicalModelName': ''}]},
-{'ip': [{'ipAdEntAddr': '1.1.1.1', 'ipAdEntNetMask': '255.255.255.255', 'ipAdEntIfIndex': 43, 'ifName': 'LoopBack0',
- 'ifAlias': '', 'ifOperStatus': 1}]},
-{'system': [{'sysDescr': 'S5720-36PC-EI-AC','sysUpTime': '17:13:37:00.37','sysName': 'zhangqiuxian'}]},
-{'interface': [{'ifIndex': 1, 'ifDescr': 'InLoopBack0', 'ifPhysAddress': '00:00:00:00:00:00', 'ifOperStatus': 1,
-'ifName': 'InLoopBack0', 'ifAlias': '', 'ifHighSpeed': 0}]}])
     """
+    snmp_objs = SnmpTemplate.objects.all()
     if snmp_objs is None or len(snmp_objs) == 0:
         return False, [{"success": {"message": f"{device.ip} SNMP模板未配置！"}}]
     snmps: list[dict] = SnmpTemplateSerializer(snmp_objs, many=True).data
@@ -56,8 +69,15 @@ def start_snmp(device: Device = None, oids: list = None) -> tuple[bool, list[dic
     s_data['hostname'] = device.ip
     s_data['oids'] = oids
     b, datas = run(**s_data)
-    datas[0].update({"ip": device.ip, "device_id": device.device_id.__str__(), "name": device.name})
-    return b, datas
+    if not b:
+        return b, datas
+    new_datas: list[dict] = []
+    for d in datas:
+        d['device_ip'] = device.ip
+        d['device_id'] = device.device_id.__str__()
+        d['device_name'] = device.name
+        new_datas.append(d)
+    return b, new_datas
 
 
 def bulk_create(model: Model, objs: list[Model]) -> None:
@@ -67,7 +87,8 @@ def bulk_create(model: Model, objs: list[Model]) -> None:
     :param objs:
     :return:
     """
-    model.objects.bulk_create(objs)
+    with transaction.atomic():
+        model.objects.bulk_create(objs)
 
 
 def bulk_update(model: Model, objs: list[Model], fields: list[str]) -> None:
@@ -78,12 +99,13 @@ def bulk_update(model: Model, objs: list[Model], fields: list[str]) -> None:
     :param fields:
     :return:
     """
-    model.objects.bulk_update(objs=objs, fields=fields)
+    with transaction.atomic():
+        model.objects.bulk_update(objs=objs, fields=fields)
 
 
-def start_thread_pool(fn: Callable, queryset: QuerySet[Model]) -> list[tuple[bool, list[dict]]]:
+def start_thread_pool(fn: Callable, queryset: QuerySet[Model], /, *args, **kwargs) -> list[tuple[bool, list[dict]]]:
     with ThreadPoolExecutor(max_workers=POOL) as executor:
-        res: Iterator[Future] = as_completed([executor.submit(fn, q) for q in queryset])
+        res: Iterator[Future] = as_completed([executor.submit(fn, q, *args, **kwargs) for q in queryset])
         response = [i.result() for i in res]
         return response
 
@@ -99,36 +121,31 @@ model_dict: dict[str, Any] = {
 }
 
 
-def update_model(device: Device) -> tuple[bool, list[dict]]:
+def create_base_model(device: Device) -> tuple[bool, list[dict]]:
     """
-    更新ip interface serial system 四个模型
+    创建ip interface serial system 四个模型
     :param device: Device对象
     :return:
     """
-
-    return start_snmp(device=device, oids=None)
-    # if not b:
-    #     return b, datas
-    # ip: list[DeviceIP] = []
-    # interface: list[DeviceInterface] = []
-    # serial: list[DeviceSerial] = []
-    # system: list[DeviceSystem] = []
-    # for data in datas:
-    #     for key, value in data.items():
-    #         obj = model_dict[key]
-    #         match key:
-    #             case 'serial':
-    #                 serial += [obj(**d, device_id=device.device_id, name=device.name, ip=device.ip) for d in value if
-    #                            d['entPhysicalSerialNum'] != '']
-    #             case 'interface':
-    #                 interface += [obj(**d, device_id=device.device_id, name=device.name, ip=device.ip) for d in value]
-    #             case 'seria':
-    #                 interface += [obj(**d, device_id=device.device_id, name=device.name, ip=device.ip) for d in value]
-    #             case 'system':
-    #                 interface += [obj(**d, device_id=device.device_id, name=device.name, ip=device.ip) for d in value]
-    # device.is_sync = True
-    # device.save()
-    # return True, [{"success": {"message": f"{device.ip} 更新完成！"}}]
+    b, datas = start_snmp(device=device, oids=None)
+    if not b:
+        return b, datas
+    keys: list[str] = ['device_ip', 'device_name', 'device_id']
+    for data in datas:
+        for key, value in data.items():
+            if key in keys:
+                pass
+            else:
+                obj = model_dict[key]
+                if key == 'serial':
+                    bulk_create(obj, [obj(**d, device_id=device.device_id, name=device.name, ip=device.ip)
+                                      for d in value if d['entPhysicalSerialNum'] != ''])
+                else:
+                    bulk_create(obj, [obj(**d, device_id=device.device_id, name=device.name, ip=device.ip)
+                                      for d in value])
+    device.is_sync = True
+    device.save()
+    return True, [{"success": {"message": f"{device.ip} 更新完成！"}}]
 
 
 @shared_task
@@ -142,22 +159,17 @@ def start_sync(*args, **kwargs) -> Task | dict:
     d_q: QuerySet[Device] = Device.objects.filter(is_sync=False).all()
     if len(d_q) == 0:
         return {"success": True, "message": "所有设备都已同步！"}
-    response: list[tuple[bool, list[dict]]] = start_thread_pool(fn=run_arp, queryset=d_q)
-    fault_list: list[dict] = []
-    error_list: list[dict] = []
-    success_list: list[dict] = []
-    create_ip: list[DeviceIP] = []
-    create_interface: list[DeviceInterface] = []
-    create_serial: list[DeviceSerial] = []
-    create_system: list[DeviceSystem] = []
+    response: list[tuple[bool, list[dict]]] = start_thread_pool(create_base_model, d_q)
+    error_count: int = 0
+    success_count: int = 0
     for r in response:
         b, m = r
         if not b:
-            error_list = error_list+m
+            error_count += 1
         else:
-            success_list = success_list+m
-    CronJobSyncLog.objects.create(job="同步基础信息", data=success_list)
-    return {"success": True, "message": f"成功了{len(success_list)}台,失败了{len(error_list)}台,未获取到{len(fault_list)}台"}
+            success_count += 1
+    CronJobSyncLog.objects.create(job="同步基础信息", data=response)
+    return {"success": True, "message": f"成功了{success_count}台,失败了{error_count}台。"}
 
 
 """
@@ -167,7 +179,7 @@ def start_sync(*args, **kwargs) -> Task | dict:
 
 def run_arp(device: Device) -> tuple[bool, list[dict]]:
     """
-    更新ip interface serial system 四个模型
+    更新arp模型
     :param device: Device对象
     :return:
     """
@@ -176,9 +188,9 @@ def run_arp(device: Device) -> tuple[bool, list[dict]]:
 
 @shared_task
 def start_sync_arp(*args, **kwargs) -> Task | dict:
-    d_q: QuerySet[Device] = Device.objects.filter(is_sync=False).all()
+    d_q: QuerySet[Device] = Device.objects.filter(is_sync=True).all()
     a_q: QuerySet[DeviceARP] = DeviceARP.objects.all()
-    response: list[tuple[bool, list[dict]]] = start_thread_pool(fn=run_arp, queryset=d_q)
+    response: list[tuple[bool, list[dict]]] = start_thread_pool(run_arp, d_q)
     fault_list: list[dict] = []
     error_list: list[dict] = []
     success_list: list[dict] = []
@@ -195,8 +207,8 @@ def start_sync_arp(*args, **kwargs) -> Task | dict:
                 fault_list = fault_list + datas
             else:
                 device_id: str = datas[0].get('device_id')
-                ip: str = datas[0].get('ip')
-                name: str = datas[0].get('name')
+                ip: str = datas[0].get('device_ip')
+                name: str = datas[0].get('device_name')
                 aq: [DeviceARP] = a_q.filter(device_id=device_id)
                 if len(aq) == 0:
                     create_models = create_models + [DeviceARP(**a, device_id=device_id, name=name, ip=ip) for a in
@@ -237,8 +249,10 @@ def start_sync_arp(*args, **kwargs) -> Task | dict:
         bulk_update(model=DeviceARP, objs=update_status, fields=fields)
     if len(create_models) > 0:
         bulk_create(model=DeviceARP, objs=create_models)
-    CronJobSyncLog.objects.create(job="更新ARP", data=success_list)
-    return {"success": True, "message": f"成功了{len(success_list)}台,失败了{len(error_list)}台,未获取到{len(fault_list)}台"}
+    CronJobSyncLog.objects.create(job="更新ARP",
+                                  data={"success": success_list, "error": error_list, "fault": fault_list})
+    return {"success": True,
+            "message": f"成功了{len(success_list)}台,失败了{len(error_list)}台,未获取到{len(fault_list)}台"}
 
 
 if __name__ == '__main__':
