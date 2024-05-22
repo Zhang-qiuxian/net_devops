206d3e01d1dc559eb2259c01b5ff48e75aa6cf20
Zhang_qiuxian <1473464511@qq.com>
2024-05-22 14:17:24 +0000

    sh脚本未完成

--- install.sh
+++ install.sh
@@ -2,20 +2,30 @@
 
 PASSWORD=$1
 
+PYTHON_VERSION='Python-3.12.3'
+NODEJS_VERSION='node-v22.2.0'
+
 SOFTWARE_DIR="/opt/software"
-PYTHON="https://mirrors.huaweicloud.com/python/3.12.3/Python-3.12.3.tar.xz"
-NODEJS="https://mirrors.huaweicloud.com/nodejs/v22.2.0/node-v22.2.0-linux-x64.tar.xz"
+PYTHON="https://mirrors.huaweicloud.com/python/3.12.3/$PYTHON_VERSION.tar.xz"
+NODEJS="https://mirrors.huaweicloud.com/nodejs/v22.2.0/$NODEJS_VERSION-linux-x64.tar.xz"
 # PYTHON="https://mirrors.huaweicloud.com/python/3.12.3/Python-3.12.3.tar.xz"
 
-function mkdir_files() {
+PWD_FILES=$(pwd)
+
+DJANGO='django_server'
+VUE='net-admin'
+
+function mkdir_files() 
+{
     echo "创建文件夹"
     echo $PASSWORD | sudo -S mkdir -p $SOFTWARE_DIR
     return 0
 }
 
-function download_package() {
+function download_package() 
+{
     # 安装Python
-    wget -O $SOFTWARE_DIR/Python3.12 $PYTHON
+    wget -O $SOFTWARE_DIR/$PYTHON_VERSION $PYTHON
     tar -xvf Python-3.12.3.tar.xz
     cd Python-3.12.3
     ./configure --prefix=/usr/local/python3
@@ -31,22 +41,43 @@ function download_package() {
     ln -s /usr/local/nodejs/bin/npm /usr/local/bin/npm
 }
 
-function install_nodejs(){
+function install_nodejs()
+{
     echo '安装Node.js'
-    sudo wget -O $SOFTWARE_DIR/node-v22.2.0-linux-x64.tar.xz $NODEJS
-    sudo tar -xvf $SOFTWARE_DIR/node-v22.2.0-linux-x64.tar.xz -C $SOFTWARE_DIR
+    sudo wget -O $SOFTWARE_DIR/$NODEJS_VERSION-linux-x64.tar.xz $NODEJS
+    sudo tar -xvf $SOFTWARE_DIR/$NODEJS_VERSION-linux-x64.tar.xz -C $SOFTWARE_DIR
     # sudo mv node-v22.2.0-linux-x64 /usr/local/nodejs
     # sudo ln -s /usr/local/nodejs/bin/node /usr/local/bin/node
 }
 
-function install_python() {
+function start_django()
+{
+    echo "开始创建django"
+}
+
+function install_python() 
+{
     # 安装Python
-    echo '安装Python'
-    sudo wget -O $SOFTWARE_DIR/Python-3.12.3.tar.xz $PYTHON
-    sudo tar -xvf $SOFTWARE_DIR/Python-3.12.3.tar.xz -C $SOFTWARE_DIR
+    echo '开始安装Python'
+
+    sudo wget -O $SOFTWARE_DIR/$PYTHON_VERSION.tar.xz $PYTHON
+    sudo tar -xvf $SOFTWARE_DIR/$PYTHON_VERSION.tar.xz -C $SOFTWARE_DIR
     # 安装Python依赖包
     # pip3 install -r requirements.txt
 }
+
+function hash_python_version(){
+    v=$(python3 -V | awk  '{print $2}' | awk -F '.' '{print $2}')
+    if [ $v -le 10 ]
+    then
+        echo "当前Python版本$v,需要升级Python版本"
+        install_python
+    else
+        echo "大于10,开始安装django虚拟环境"
+    fi
+}
+
+
 
 function main() {
     
@@ -55,8 +86,9 @@ function main() {
     
     echo $PASSWORD
     mkdir_files
-    install_python
-    install_nodejs
+    start_django
+    # install_python
+    # install_nodejs
     # if [ "$ROOT" != "root" ]; then
     #     echo "请使用root用户运行"
     #     echo $ROOT
@@ -64,7 +96,7 @@ function main() {
 
 }
 
-main $
+main
 
 # function download_package() {
 #     # 安装MariaDB
